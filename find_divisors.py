67365d57c660297dd3ee09d82d9c2b6875942e8f
NONEZONYX <108766995+nonezonyx@users.noreply.github.com>
2022-08-10 23:30:20 +0300

    Update find_divisors.py

--- find_divisors.py
+++ find_divisors.py
@@ -1,10 +1,10 @@
 def find_divisors(num: int): #num>0
     x=round(num**0.5)
     ans=[]
-    while x<=num:
+    while x:
         if num%x==0:
             ans.append((x,num//x))
-        x+=1
+        x-=1
     return ans
 
 def main():
