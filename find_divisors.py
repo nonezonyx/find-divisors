b56b3afef64f4562d1034044a21c446fd96c10b4
NONEZONYX <108766995+nonezonyx@users.noreply.github.com>
2022-11-03 21:52:15 +0300

    Update find_divisors.py
    simplified function and added documentation

--- find_divisors.py
+++ find_divisors.py
@@ -1,11 +1,11 @@
-def find_divisors(num: int): #num>0
-    x=round(num**0.5)
-    ans=[]
-    while x:
-        if num%x==0:
-            ans.append((x,num//x))
-        x-=1
-    return ans
+def find_divisors(num: int) -> list[tuple[int,int]]:
+    """find_divisors(num: int) -> list[tuple[int,int]]:
+finds integer devisors of num and returns list of tuple of divisors
+
+Example:
+>>>find_divisors(10)
+[(1,10)(2,5)]"""
+    return [(i,num//i) for i in range(1,int(num**0.5)+1) if not num%i]
 
 def main():
     try:
